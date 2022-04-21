81fcf48b8eb259435ed8f5fd84f0786f30545892
faisaltimbangan32@gmail.com <faisaltimbangan32@gmail.com>
2022-04-21 09:00:31 +0700

    M. Faisal_191011400061_UTS_Kecerdasan Buatan

---
+++ main.py
@@ -0,0 +1,58 @@
+# Perulangan (loop)
+
+# for kondisi:
+# 	aksi
+
+# ini dengan list
+angka2_list = [0,2,4,8,10] # ini adalah list
+print(angka2_list)
+
+for i in angka2_list:
+	print(f"i sekarang -> {i}")
+
+print("akhir dari program 1 \n")
+
+# ini dengan range
+angka2_range = range(5)
+
+for i in angka2_range:
+	print(f"i sekarang -> {i}")
+
+print("akhir dari program 2 \n")
+
+angka2_range = range(1,10)
+
+for i in angka2_range:
+	print(f"i sekarang -> {i}")
+	
+
+print("akhir dari program 3 \n")
+
+# menggunakan string
+
+data_str = "saya ganteng abiees"
+
+for huruf in data_str:
+	print(huruf)
+
+print("akhir dari program 4 \n")
+
+
+
+# while loop
+
+# while kondisi:
+# 	aksi ini
+# 	aksi itu
+
+# akhir dari program
+
+angka = 0
+print(f"angka sekarang -> {angka}")
+
+while angka < 5:
+	angka += 1
+	print(f"angka sekarang -> {angka}")
+	print("aku ganteng maxsyimaal!")
+
+print("cukuuup")
